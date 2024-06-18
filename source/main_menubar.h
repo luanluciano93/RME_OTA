ed6f2078677f0c0ad35f1a47d29118609455f1e5
Luan Luciano <luanluciano@outlook.com>
2024-06-17 21:26:55 -0300

    Update main_menubar.h

--- source/main_menubar.h
+++ source/main_menubar.h
@@ -154,6 +154,10 @@ namespace MenuBar {
 		EXTENSIONS,
 		GOTO_WEBSITE,
 		ABOUT,
+		SEARCH_ON_MAP_DUPLICATE,
+		SEARCH_ON_SELECTION_DUPLICATE,
+		REMOVE_ON_MAP_DUPLICATE_ITEMS,
+		REMOVE_ON_SELECTION_DUPLICATE_ITEMS,
 
 		EXPERIMENTAL_FOG,
 	};
@@ -288,12 +292,20 @@ public:
 	void OnGotoWebsite(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 
+	void OnSearchForDuplicateItemsOnMap(wxCommandEvent& event);
+	void OnSearchForDuplicateItemsOnSelection(wxCommandEvent& event);
+	void OnRemoveForDuplicateItemsOnMap(wxCommandEvent& event);
+	void OnRemoveForDuplicateItemsOnSelection(wxCommandEvent& event);
+
 protected:
 	// Load and returns a menu item, also sets accelerator
 	wxObject* LoadItem(pugi::xml_node node, wxMenu* parent, wxArrayString& warnings, wxString& error);
 	// Checks the items in the menus according to the settings (in config)
 	void LoadValues();
 	void SearchItems(bool unique, bool action, bool container, bool writable, bool onSelection = false);
+
+	void SearchDuplicatedItems(bool onSelection = false);
+	void RemoveDuplicatesItems(bool onSelection = false);
 
 protected:
 	MainFrame* frame;
